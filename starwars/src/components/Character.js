ba5eb0ba616c7c04c746a3aef64df15c0d82c698
Dawna Backmeier <karanacesin@yahoo.com>
2020-05-18 19:56:39 -0400

    modified

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,18 @@
-// Write your Character component here
+import React from 'react';
+
+const Character = props => {
+
+    return (
+        <li key = {props.index}>
+            <h2>{props.name}</h2>
+            <p>{props.gender}</p>
+            <p>{props.birth}</p>
+            <p>{props.hair}</p>
+            <p>{props.skin}</p>
+            <p>{props.eyes}</p>
+        </li>
+    )
+
+}
+
+export default Character;
